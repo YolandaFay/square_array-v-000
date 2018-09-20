38ebb1932aa16827fcae9a11f45768fa6e869f57
Yolanda Fay <yolandafay@gmail.com>
2018-09-20 02:17:55 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
-  #counter = 0 
-  #array.each do | entry |
-    #array[counter] = entry**2 
-    #counter += 1
-  #end
+  counter = 0 
+  array.each do | entry |
+    array[counter] = entry**2 
+    counter += 1
+  end
   
-  array.collect{|entry| entry**2}
+  #array.collect{|entry| entry**2}
 end
